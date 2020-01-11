aa2b9bfa211be90eb806a2a889b33c58b73f71ef
athang213 <athang213@athang213-Lenovo-G50-80>
2020-01-11 19:29:46 +0530

    File Upload Complete

--- src/main/java/com/xdevapps/navigable/planuploader/PlanUploader.java
+++ src/main/java/com/xdevapps/navigable/planuploader/PlanUploader.java
@@ -5,10 +5,12 @@
  */
 package com.xdevapps.navigable.planuploader;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Stylesheet;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
@@ -21,6 +23,8 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import javax.swing.AbstractButton;
+import javax.swing.ButtonModel;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
@@ -31,8 +35,7 @@ import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.JToolBar;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
-import jdk.nashorn.internal.codegen.CompilerConstants;
+import javax.swing.plaf.ButtonUI;
 
 /**
  *
@@ -45,17 +48,17 @@ public class PlanUploader extends JFrame implements MouseListener {
     JTextPane statusPanel;
     StorageHandler storageHandler;
     int srcPt;
-    boolean isDeleteOn = false;
     boolean isSrcSet = false;
     
     public static enum DrawMode {
-        REGION, EDGE
+        REGION, EDGE,SETAXIS,DELETE
     }
     
     private DrawMode drawMode = DrawMode.REGION;
     
     public PlanUploader(){
-        JFileChooser fileChooser = new JFileChooser("/home/shubham/Documents/");
+        
+        JFileChooser fileChooser = new JFileChooser("/home/athang213/Downloads/");
         FileNameExtensionFilter filter = new FileNameExtensionFilter("jpeg", "jpg","png","bmp");
         fileChooser.setFileFilter(filter);
             if(fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
@@ -68,11 +71,11 @@ public class PlanUploader extends JFrame implements MouseListener {
             }
         }
         
-      /*  try {
-            storageHandler = new StorageHandler();
+        try {
+                storageHandler = new StorageHandler();
         } catch (IOException ex) {
             Logger.getLogger(PlanUploader.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
             
         addMouseListener(this);
         
@@ -81,8 +84,14 @@ public class PlanUploader extends JFrame implements MouseListener {
         toolBar.setPreferredSize(new Dimension(1280, 30));
         add(toolBar, BorderLayout.PAGE_START);
         
-        JButton toggleButton = new JButton("Draw Edges");
-        toolBar.add(toggleButton);
+        JButton setOriginButton = new JButton("Set Origin");
+        toolBar.add(setOriginButton);
+        
+        JButton drawRegionButton = new JButton("Draw Region");
+        toolBar.add(drawRegionButton);
+        
+        JButton drawEdgeButton = new JButton("Draw Edges");
+        toolBar.add(drawEdgeButton);
         
         JButton saveButton = new JButton("Save Data");
         toolBar.add(saveButton);
@@ -97,14 +106,17 @@ public class PlanUploader extends JFrame implements MouseListener {
         statusPanel.setText("src:");
         toolBar.add(statusPanel);
         
-        toggleButton.addActionListener(new ActionListener() {
+        drawEdgeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                toggleMode();
-                if(toggleButton.getText().contains("Region"))
-                    toggleButton.setText("Draw Edges");
-                else if(toggleButton.getText().contains("Edges"))
-                    toggleButton.setText("Draw Regions");
+                toggleMode("EDGE");
+            }
+        });
+        
+        drawRegionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                toggleMode("REGION");
             }
         });
         
@@ -112,7 +124,8 @@ public class PlanUploader extends JFrame implements MouseListener {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 try{
-                    imagePanel.writeToFile();
+                    byte[] bytes = imagePanel.writeToFile();
+                    storageHandler.uploadFile(bytes, bytes.length);
                     JOptionPane.showMessageDialog(getContentPane(), "Saved Successfully");
                 }catch(IOException e){
                     e.printStackTrace();
@@ -125,6 +138,12 @@ public class PlanUploader extends JFrame implements MouseListener {
         resetButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
+                
+                /*AbstractButton b = (AbstractButton) ae.getSource();
+                ButtonModel m = b.getModel();
+                m.setPressed(true);
+                 if (! b.isFocusOwner())
+                    b.requestFocus();*/
                 imagePanel.reset();
                 repaint();
             }
@@ -133,10 +152,18 @@ public class PlanUploader extends JFrame implements MouseListener {
         deleteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-            	toggleDelete(); 
+            	toggleMode("DELETE"); 
             }
         });
        
+        setOriginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                
+                toggleMode("SETAXIS");
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
         
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -158,43 +185,41 @@ public class PlanUploader extends JFrame implements MouseListener {
     @Override
     public void mousePressed(MouseEvent me) {
         int x = me.getX(); 
-        int y = me.getY()-58;
+        int y = me.getY()-30;
         
-        if(isDeleteOn == true){	
-        	
-        	imagePanel.deletePolygon(x,y);	
-        	
-        	repaint();
+	if(drawMode == DrawMode.REGION){
+	    if(me.getButton() == MouseEvent.BUTTON1) {
+	        imagePanel.drawPoint(x, y);
+	        repaint();
+	    } else if(me.getButton() == MouseEvent.BUTTON3) {
+	        imagePanel.drawPolygon();
+	        repaint();
+	    }
+	} else if(drawMode == DrawMode.EDGE){
+	    if(me.getButton() == MouseEvent.BUTTON1) {
+	        int closestPoint = imagePanel.isCloseTo(x, y);
+	        statusPanel.setText("closest:" + closestPoint);
+	        if(closestPoint != -1){
+	            if(isSrcSet) {
+                        imagePanel.highlightPt(closestPoint);
+	                imagePanel.drawEdge(srcPt, closestPoint);
+	            }
+	            else {
+	                srcPt = closestPoint;
+                        imagePanel.highlightPt(closestPoint);
+	                statusPanel.setText("src:" + srcPt);
+	            }
+	            isSrcSet = !isSrcSet;
+	            repaint();
+	        }
+	    } 
+	} else if(drawMode == DrawMode.SETAXIS){
+            imagePanel.setAxis(x,y);
+            repaint();
+        } else if(drawMode == DrawMode.DELETE){
+            imagePanel.deletePolygon(x,y);	
+            repaint();
         }
-        else {
-	        if(drawMode == DrawMode.REGION){
-	            if(me.getButton() == MouseEvent.BUTTON1) {
-	                imagePanel.drawPoint(x, y);
-	                repaint();
-	            } else if(me.getButton() == MouseEvent.BUTTON3) {
-	                imagePanel.drawPolygon();
-	                repaint();
-	            }
-	        } else if(drawMode == DrawMode.EDGE){
-	            if(me.getButton() == MouseEvent.BUTTON1) {
-	                int closestPoint = imagePanel.isCloseTo(x, y);
-	                statusPanel.setText("closest:" + closestPoint);
-	                if(closestPoint != -1){
-	                    if(isSrcSet) {
-	                        imagePanel.drawEdge(srcPt, closestPoint);
-	                    }
-	                    else {
-	                        srcPt = closestPoint;
-	                        statusPanel.setText("src:" + srcPt);
-	                    }
-	                    isSrcSet = !isSrcSet;
-	                    repaint();
-	                }
-	            } else if(me.getButton() == MouseEvent.BUTTON3) {
-	                
-	            }
-	        } 
-        }   
     }
 
     @Override
@@ -204,7 +229,7 @@ public class PlanUploader extends JFrame implements MouseListener {
 
     @Override
     public void mouseEntered(MouseEvent me) {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     @Override
@@ -212,24 +237,18 @@ public class PlanUploader extends JFrame implements MouseListener {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
-    void toggleMode() {
-        if(drawMode == DrawMode.REGION) {
+    void toggleMode(String mode) {
+       if (mode.equals("EDGE")) {
             drawMode = DrawMode.EDGE;
-        }
-        else {
+        }else if (mode.equals("REGION")) {
             drawMode = DrawMode.REGION;
-        }
+        }else if (mode.equals("SETAXIS")) {
+            drawMode = DrawMode.SETAXIS; 
+        }else if (mode.equals("DELETE")) {
+            drawMode = DrawMode.DELETE;   
+        } 
     }
     
-    void toggleDelete() {
-    	if(isDeleteOn == true)
-    	{
-    		isDeleteOn = false;
-    	}
-    	else {
-    		isDeleteOn = true;
-    	}
-    }
     
     public static void main(String[] args) {
         PlanUploader planUploader = new PlanUploader();
